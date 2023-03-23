99a32c39d375ef616810f77eda517f653c44eb8b
sagedev0429 <topdevprince@gmail.com>
2023-03-23 18:26:39 +0300

    bloc complete

--- lib/features/administration/masters/awareness_groups/bloc/awareness_groups_event.dart
+++ lib/features/administration/masters/awareness_groups/bloc/awareness_groups_event.dart
@@ -4,7 +4,69 @@ abstract class AwarenessGroupsEvent extends Equatable {
   const AwarenessGroupsEvent();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
 
 class AwarenessGroupsRetrieved extends AwarenessGroupsEvent {}
+
+class AwarenessGroupSelected extends AwarenessGroupsEvent {
+  final AwarenessGroup? awarenessGroup;
+  const AwarenessGroupSelected({
+    this.awarenessGroup,
+  });
+  @override
+  List<Object?> get props => [
+        awarenessGroup,
+      ];
+}
+
+class AwarenessGroupSelectedById extends AwarenessGroupsEvent {
+  final String awarenessGroupId;
+  const AwarenessGroupSelectedById({
+    required this.awarenessGroupId,
+  });
+  @override
+  List<Object> get props => [
+        awarenessGroupId,
+      ];
+}
+
+class AwarenessGroupAdded extends AwarenessGroupsEvent {
+  final AwarenessGroup awarenessGroup;
+  const AwarenessGroupAdded({
+    required this.awarenessGroup,
+  });
+
+  @override
+  List<Object> get props => [
+        awarenessGroup,
+      ];
+}
+
+class AwarenessGroupEdited extends AwarenessGroupsEvent {
+  final AwarenessGroup awarenessGroup;
+  const AwarenessGroupEdited({
+    required this.awarenessGroup,
+  });
+
+  @override
+  List<Object> get props => [
+        awarenessGroup,
+      ];
+}
+
+class AwarenessGroupDeleted extends AwarenessGroupsEvent {
+  final String awarenessGroupId;
+  const AwarenessGroupDeleted({
+    required this.awarenessGroupId,
+  });
+
+  @override
+  List<Object> get props => [
+        awarenessGroupId,
+      ];
+}
+
+class AwarenessGroupsStatusInited extends AwarenessGroupsEvent {
+  const AwarenessGroupsStatusInited();
+}
