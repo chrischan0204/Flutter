c8bb24d247f65a8d3f527f711653624586fe2a4c
sagedev0429 <topdevprince@gmail.com>
2023-03-24 12:44:15 +0300

    bloc complete

--- lib/features/administration/masters/priority_levels/bloc/priority_levels_event.dart
+++ lib/features/administration/masters/priority_levels/bloc/priority_levels_event.dart
@@ -4,7 +4,69 @@ abstract class PriorityLevelsEvent extends Equatable {
   const PriorityLevelsEvent();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
 
 class PriorityLevelsRetrieved extends PriorityLevelsEvent {}
+
+class PriorityLevelSelected extends PriorityLevelsEvent {
+  final PriorityLevel? priorityLevel;
+  const PriorityLevelSelected({
+    this.priorityLevel,
+  });
+  @override
+  List<Object?> get props => [
+        priorityLevel,
+      ];
+}
+
+class PriorityLevelSelectedById extends PriorityLevelsEvent {
+  final String priorityLevelId;
+  const PriorityLevelSelectedById({
+    required this.priorityLevelId,
+  });
+  @override
+  List<Object> get props => [
+        priorityLevelId,
+      ];
+}
+
+class PriorityLevelAdded extends PriorityLevelsEvent {
+  final PriorityLevel priorityLevel;
+  const PriorityLevelAdded({
+    required this.priorityLevel,
+  });
+
+  @override
+  List<Object> get props => [
+        priorityLevel,
+      ];
+}
+
+class PriorityLevelEdited extends PriorityLevelsEvent {
+  final PriorityLevel priorityLevel;
+  const PriorityLevelEdited({
+    required this.priorityLevel,
+  });
+
+  @override
+  List<Object> get props => [
+        priorityLevel,
+      ];
+}
+
+class PriorityLevelDeleted extends PriorityLevelsEvent {
+  final String priorityLevelId;
+  const PriorityLevelDeleted({
+    required this.priorityLevelId,
+  });
+
+  @override
+  List<Object> get props => [
+        priorityLevelId,
+      ];
+}
+
+class PriorityLevelsStatusInited extends PriorityLevelsEvent {
+  const PriorityLevelsStatusInited();
+}
