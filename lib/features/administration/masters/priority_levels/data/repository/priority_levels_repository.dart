2fb7ed11d13ffdf01f2d5af924fef640e8f8bd51
sagedev0429 <topdevprince@gmail.com>
2023-03-24 12:33:18 +0300

    priority level repository

--- lib/features/administration/masters/priority_levels/data/repository/priority_levels_repository.dart
+++ lib/features/administration/masters/priority_levels/data/repository/priority_levels_repository.dart
@@ -3,11 +3,13 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:safety_eta/constants/uri.dart';
 
+import '/data/model/entity.dart';
 import '../model/priority_level.dart';
 
 class PriorityLevelsRepository {
+  static String url = '/api/PriorityLevels';
   Future<List<PriorityLevel>> getPriorityLevels() async {
-    Response response = await get(ApiUri.getPrioritiesUri);
+    Response response = await get(Uri.https(ApiUri.host, url));
     if (response.statusCode == 200) {
       List<PriorityLevel> priorityLevels = List.from(jsonDecode(response.body))
           .map(
@@ -17,5 +19,62 @@ class PriorityLevelsRepository {
       return priorityLevels;
     }
     return [];
+  }
+
+  Future<PriorityLevel> getPriorityLevelById(
+    String priorityLevelId,
+  ) async {
+    Response response =
+        await get(Uri.https(ApiUri.host, '$url/$priorityLevelId'));
+
+    if (response.statusCode == 200) {
+      return PriorityLevel.fromJson(response.body);
+    }
+    throw Exception('');
+  }
+
+  Future<EntityResponse> addPriorityLevel(
+    PriorityLevel priorityLevel,
+  ) async {
+    Response response = await post(
+      Uri.https(ApiUri.host, url),
+      headers: {
+        'Content-Type': 'application/json',
+        'accept': 'text/plain',
+      },
+      body: priorityLevel.toJson(),
+    );
+
+    if (response.statusCode == 200) {
+      return EntityResponse.fromJson(response.body);
+    }
+    throw Exception();
+  }
+
+  Future<EntityResponse> editPriorityLevel(
+    PriorityLevel priorityLevel,
+  ) async {
+    Response response = await put(Uri.https(ApiUri.host, url),
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json',
+        },
+        body: priorityLevel.toJson());
+
+    if (response.statusCode == 200) {
+      return EntityResponse.fromJson(response.body);
+    }
+    throw Exception();
+  }
+
+  Future<EntityResponse> deletePriorityLevel(String priorityLevelId) async {
+    Response response = await delete(
+      Uri.https(ApiUri.host, '$url/$priorityLevelId'),
+    );
+
+    if (response.statusCode == 200) {
+      return EntityResponse.fromJson(response.body);
+    }
+    throw Exception();
   }
 }
