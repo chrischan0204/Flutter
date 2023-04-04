7fac31526e56cac4152e5c8d92a3eb3a4c114547
sagedev0429 <topdevprince@gmail.com>
2023-04-04 17:44:41 +0300

    router setting

--- lib/features/administration/projects/projects.dart
+++ lib/features/administration/projects/projects.dart
@@ -1,1 +1,2 @@
 export 'view/projects_list.dart';
+export 'view/add_edit_project.dart';
---
+++ lib/features/administration/projects/view/add_edit_project.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AddEditProjectView extends StatefulWidget {
+  final String? projectId;
+  const AddEditProjectView({
+    super.key,
+    this.projectId,
+  });
+
+  @override
+  State<AddEditProjectView> createState() => _AddEditProjectViewState();
+}
+
+class _AddEditProjectViewState extends State<AddEditProjectView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
